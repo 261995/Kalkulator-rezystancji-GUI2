b09183b5de0858a4c60ff23391ea2f5d32116cec
pwr357802 <261995@student.pwr.edu.pl>
2021-12-03 01:34:33 +0100

    03.12.2021 01:34

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include "mainwindow.h"
-
+#include <iostream>
 #include <QApplication>
+#include <vector>
 
 int main(int argc, char *argv[])
 {
@@ -9,3 +10,32 @@ int main(int argc, char *argv[])
     w.show();
     return a.exec();
 }
+
+class Dzialania
+{
+    QVector<float> szeregowo;
+    QVector<float> rownolegle;
+    QList<QString> nazwyR = {double R1, double R2, double R3, double R4, double R5};
+
+    void dodaj_szeregowo(QVector<float> szeregowo, QList<QString> nazwyR)
+    {
+        for (int i = 0; i < nazwyR.size(); i++)
+            if (nazwyR.at(i) != "")
+                szeregowo.push_back(nazwyR.at(i));
+            else
+                return
+    }
+
+    void dodaj_rownolegle(QVector<float> rownolegle, QList<QString> nazwyR)
+    {
+        for (int i = 0; i < nazwyR.size(); i++)
+            if (nazwyR.at(i) != "")
+                rownolegle.push_back(nazwyR.at(i));
+            else
+                return
+    }
+
+    int size1 = szeregowo.size();
+    int size2 = rownolegle.size();
+
+};
