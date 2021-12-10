ab6d48655411ede1b47d89fab6b161eea722efc0
pwr357802 <261995@student.pwr.edu.pl>
2021-12-10 07:33:46 +0100

    07:33 10.12.2021

--- dzialania.h
+++ dzialania.h
@@ -17,15 +17,12 @@ class Dzialania
 
         void dodaj_rownolegle(QList<double> rezystory_R);
 
-
         void dodawanie_szeregowo(double SumaS);
 
         void dodawanie_rownolegle(double SumaR);
 
-
-    private:
-
         void constr_func(double R1, double R2, double R3, double R4, double R5);
+
         void constr_func2(double R1_2, double R2_2, double R3_2, double R4_2, double R5_2);
 
 
